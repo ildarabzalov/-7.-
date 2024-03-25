9b17f68d639f92f0e9077d2cf4cf9e554890d8a0
ildar.abzalov <abzalov.ildar@bk.ru>
2024-03-25 23:20:48 +0500

    Третья задача

--- Program.cs
+++ Program.cs
@@ -16,19 +16,35 @@
 
 // Задача 2: Напишите программу вычисления функции
 // Аккермана с помощью рекурсии. Даны два
-// неотрицательных числа m и n.
-int Accerman(int m, int n)
-{
-    if (m == 0)
-        return (n + 1);
-    else if (n == 0)
-        return Accerman(m - 1, 1);
-    else
-        return Accerman(m - 1, Accerman(m, n - 1));
-}
+// // неотрицательных числа m и n.
+// int Accerman(int m, int n)
+// {
+//     if (m == 0)
+//         return (n + 1);
+//     else if (n == 0)
+//         return Accerman(m - 1, 1);
+//     else
+//         return Accerman(m - 1, Accerman(m, n - 1));
+// }
 
-Console.Write("ВВедите число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("ВВедите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Accerman(m, n));
+// Console.Write("ВВедите число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("ВВедите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Accerman(m, n));
+
+// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+void PrintArrayReverse(int[] array, int index)
+    {
+        if (index < 0)
+            return;
+        Console.Write(array[index] + " ");
+        PrintArrayReverse(array, index - 1);
+    }
+
+        int[] arr = { 1, 2, 5, 0, 10, 34 };
+        Console.WriteLine("Элементы массива с конца:");
+        PrintArrayReverse(arr, arr.Length - 1);
